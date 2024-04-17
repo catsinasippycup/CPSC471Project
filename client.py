0bbd7d5a79404aea7a6eceeb42f6c1d6bb0d2dc6
James Peou <47491266+catsinasippycup@users.noreply.github.com>
2024-04-17 03:03:20 -0700

    added: get command

--- client.py
+++ client.py
@@ -2,6 +2,28 @@ import sys
 import os
 import socket
 
+def recvAll(sock, numBytes):
+
+	# The buffer
+	recvBuff = ""
+	
+	# The temporary buffer
+	tmpBuff = ""
+	# Keep receiving till all is received
+	while len(recvBuff) < numBytes:
+		
+		# Attempt to receive bytes
+		tmpBuff =  sock.recv(numBytes)
+		
+		# The other side has closed the socket
+		if not tmpBuff:
+			break
+		
+		# Add the received bytes to the buffer
+		recvBuff += tmpBuff.decode()
+	
+	return recvBuff
+
 # Command line checks 
 if len(sys.argv) != 3:
 	print ("USAGE python " + sys.argv[0] + " <SERVER ADDRESS> <SERVER PORT>" )
@@ -16,11 +38,41 @@ print("Connected to server")
  
 while True:
     command = input("ftp> ")
-    connSock.send(command.encode())
-
-    if command.lower() == 'quit':
+    
+    if command.startswith('get'):
+        
+        connSock.send(command.encode())
+        
+        # The buffer to all data received from the client.
+        fileData = ""
+        
+        # The temporary buffer to store the received data.
+        recvBuff = ""
+        
+        # The size of the incoming file
+        fileSize = 0
+        
+        # The buffer containing the file size
+        fileSizeBuff = ""
+        
+        # Receive the first 10 bytes indicating the size of the file
+        fileSizeBuff = recvAll(connSock, 10)
+        
+        # Get the file size
+        fileSize = int(fileSizeBuff)
+        
+        print (f"The file size is  {fileSize} bytes" )
+        
+        # Get the file data
+        fileData = recvAll(connSock, fileSize)
+        
+        print ("The file data is: ")
+        print (fileData, "\n")
+        
+    elif command.startswith('quit'):
         print("Disconnecting")
         break
+     
 
 print("Socket closing")   
 connSock.close()
--- server.py
+++ server.py
@@ -2,13 +2,34 @@ import sys
 import os
 import socket
 
+def recvAll(sock, numBytes):
+    # The buffer
+    recvBuff = ""
+    
+    tmpBuff = ""
+    
+    # Keep receiving till all is received
+    while len(recvBuff) < numBytes:
+        
+        # Attempt to receive bytes
+        tmpBuff =  sock.recv(numBytes)
+        
+        # The other side has closed the socket
+        if not tmpBuff:
+            break
+        
+        # Add the received bytes to the buffer
+        recvBuff += tmpBuff.decode()
+    
+    return recvBuff
+
 # Command line argument check
 if len(sys.argv) != 2:
-    print("USAGE: " + sys.argv[0] + "<SERVER PORT>")
+    print("USAGE: " + sys.argv[0] + " <SERVER PORT>")
     sys.exit(1)
 
 # The port on which to listen
-listenPort = int(sys.argv[1])		
+listenPort = int(sys.argv[1])        
 
 # Create a welcome socket
 welcomeSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,7 +42,47 @@ welcomeSock.listen(1)
 
 print("Server listening on port", listenPort)
 
+# Accept the connection
 client_socket, client_address = welcomeSock.accept()
 print(f"Connection from {client_address} established.")
 
+# Keep sending until all is sent
+while True:
+    # Receive the data
+    command = client_socket.recv(1024).decode()
+    
+    if command.startswith("get"):
+        fileName = command[4:]
+        try:
+            with open(fileName, "r") as fileObj:
+                # Read 65536 bytes of data
+                fileData = fileObj.read(65536)
+                # Make sure we did not hit EOF
+                if fileData:
+                    # Get the size of the data read and convert it to string
+                    dataSizeStr = str(len(fileData))
+                    
+                    # Prepend 0's to the size string until the size is 10 bytes
+                    while len(dataSizeStr) < 10:
+                        dataSizeStr = "0" + dataSizeStr
+                    # Prepend the size of the data to the file data.
+                    fileData = dataSizeStr + fileData
+                    # The number of bytes sent
+                    numSent = 0
+                    # Send the data!
+                    while len(fileData) > numSent:
+                        numSent += client_socket.send(fileData[numSent:].encode())
+                # The file has been read. We are done
+                else:
+                    break
+                print ("Sent ", numSent, " bytes.")
+                print ("Get command success")
+
+        except FileNotFoundError:
+            print("File not found")
+    else:
+        print(f"Client: {client_address} disconnected")
+        break
+
+# Close the connection
 client_socket.close()
